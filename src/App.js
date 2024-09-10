299d9485e12c321ba868282026d05f4c911d2953
JarupongMin <129025834+mdeenit@users.noreply.github.com>
2024-09-10 20:27:06 +1000

    update

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+const food = [
+  "bananas",
+  "bacon", 
+  "mango", 
+  "nachos",
+  "KFC", 
+  "sushi",
+  "tinned spaghetti"
+]
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+
+    <ul>
+      {
+        food.map(individualFoodItem => {
+          return <li>
+            {individualFoodItem}
+          </li>
+        })
+      }
+    </ul>
+
+    </div>
+  );
+}
+
+export default App;
